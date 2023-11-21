e6d8ca860a8161fc538ef0a41b26808dbf7c7555
aaix <55102797+aaix@users.noreply.github.com>
2023-11-21 03:25:49 +0000

    send cors info on every request

--- api/context.php
+++ api/context.php
@@ -45,22 +45,23 @@ function contextual_run() {
     $args = $routing_params[1] ?? null;
 
     // CORS PREFLIGHT CHECK DO OUR CORS CONTROL HERE
+    $origin = $_SERVER["HTTP_ORIGIN"] ?? null;
+
+    // if the origin is some form of localhost allow it
+    // otherwise we only allow the root domain
+
+    if (preg_match("/^https?:\/\/localhost(:[0-9]{0,5})?$/", $origin)) {
+        header("Access-Control-Allow-Origin: ". $origin);
+    } else {
+        header("Access-Control-Allow-Origin: https://013.team");
+    }
+
+    header("Access-Control-Allow-Methods: ". implode(", ", $route->allowed_methods));
+    header("Access-Control-Allow-Headers: content-type");
+    header("Access-Control-Allow-Credentials: true");
+    // header("Access-Control-Max-Age: 86400"); // 1 day of access control
+
     if ($ctx->request_method == "OPTIONS") {
-
-        $origin = $_SERVER["HTTP_ORIGIN"] ?? null;
-
-        // if the origin is some form of localhost allow it
-        // otherwise we only allow the root domain
-
-        if (preg_match("/^https?:\/\/localhost(:[0-9]{0,5})?$/", $origin)) {
-            header("Access-Control-Allow-Origin: ". $origin);
-        } else {
-            header("Access-Control-Allow-Origin: https://013.team");
-        }
-
-        header("Access-Control-Allow-Methods: ". implode(", ", $route->allowed_methods));
-        header("Access-Control-Allow-Headers: content-type");
-        header("Access-Control-Allow-Credentials: true");
         respond_no_content();
     }
 
