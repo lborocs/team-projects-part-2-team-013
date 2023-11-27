031c0d3b94ceb970a68af3c794b48d84300fc9bc
aaix <55102797+aaix@users.noreply.github.com>
2023-11-27 20:13:52 +0000

    only try to decode if body exists

--- api/common.php
+++ api/common.php
@@ -67,9 +67,12 @@ class RequestContext extends stdClass {
             $this->auth_level = 0;
         }
         
+
+        $body = file_get_contents('php://input');
+
         // parse request body
-        if ($this->method_allows_body()) {
-            $json = json_decode(file_get_contents('php://input'), true);
+        if ($this->method_allows_body() && $body) {
+            $json = json_decode($body, true);
 
             // only error if the body is invalid json, not for null bodies
             if (is_null($json) && json_last_error() !== JSON_ERROR_NONE) {
