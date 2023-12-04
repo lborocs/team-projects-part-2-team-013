559f2b3e75099abe91c5468c0846e0577fc7915f
aaix <55102797+aaix@users.noreply.github.com>
2023-12-04 21:46:03 +0000

    create a db bacvkup script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TIME=`date +%Y-%m-%d--%H-%M-%S`
+echo "Backup started at $TIME"
+sudo mariadb-dump team013 > dumps/$TIME.sql
+echo "Uploading backup to GCP"
+gsutil cp dumps/$TIME.sql gs://backups.013.team/
+TIME=`date +%Y-%m-%d--%H-%M-%S`
+echo "Backup completed at $TIME"
