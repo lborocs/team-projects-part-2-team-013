444e3e59acb09cd5843aef41b9daecdbc0e2dd00
aaix <55102797+aaix@users.noreply.github.com>
2023-12-04 20:44:35 +0000

    remove old debug from assets testing

--- api/routes/debug.php
+++ api/routes/debug.php
@@ -1,8 +1,3 @@
 <?php
-require_once("lib/assets/asset.php");
-require_once("lib/database.php");
-$asset = Asset::create(file_get_contents("https://i.pinimg.com/564x/a3/d5/e9/a3d5e93f5ab1980af4705c080f7c6f26.jpg"), ASSET_TYPE::USER_AVATAR, "1111");
-
-
 phpinfo();
 ?>
