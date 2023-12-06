b8b5753339fb29457e93d41b41a8f20a0d392b2e
fakeishere <meh.usman321@gmail.com>
2023-12-06 16:25:04 +0000

    further updates to js to remove in progress and completed add task buttons

--- web/projects/client.js
+++ web/projects/client.js
@@ -37,8 +37,6 @@ const notStartedColumn = document.querySelector("#notstarted")
 const inProgressColumn = document.querySelector("#inprogress")
 const finishedColumn = document.querySelector("#finished")
 const notStartedAddButton = document.querySelector("#notstarted-add")
-const inProgressAddButton = document.querySelector("#inprogress-add")
-const finishedAddButton = document.querySelector("#finished-add")
 const listAddButtonRow = document.querySelector("#list-add-row")
 const listAddButton = document.querySelector("#list-add")
 const projectBackButton = document.querySelector("#project-back")
@@ -1080,16 +1078,12 @@ function updateAssignedEmployees(element, assignedSet, employeeMap) {
     });
 }
 
-const addButtonArray = [notStartedAddButton, inProgressAddButton, finishedAddButton];
+const addButtonArray = [notStartedAddButton];
 
 addButtonArray.forEach((button) => {
     button.addEventListener("click", async () => {
         if (button.id == "notstarted-add") {
             await addTask(0);
-        } else if (button.id == "inprogress-add") {
-            await addTask(1);
-        } else if (button.id == "finished-add") {
-            await addTask(2);
         } else {
             console.error("invalid state");
         }
