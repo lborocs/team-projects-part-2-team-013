3270f03b5b887e917090854feda46c1cb24bad69
TRGRally <jamiespare1@gmail.com>
2024-01-08 02:53:13 +0000

    progress on ui functionality for manager dashboard

--- web/dashboard/client.js
+++ web/dashboard/client.js
@@ -6,7 +6,12 @@ console.log("loaded client.js")
 const dashboardContainer = document.querySelector(".dashboard-container")
 const gridExpand = document.querySelector("#grid-expand")
 const gridShrink = document.querySelector("#grid-shrink")
-const gridReset = document.querySelector("#grid-reset")
+const gridUndo = document.querySelector("#grid-undo")
+const viewScroll = document.querySelector("#view-scroll")
+const viewFit = document.querySelector("#view-fit")
+const addMetricButton = document.querySelector(".metric-card.add-metric")
+
+let gridActionsQueue = []
 
 class Dashboard {
     constructor() {
@@ -336,29 +341,255 @@ charts.push(new Chart(document.getElementById("timeBudgetChart"), {
 
 
 
+async function addMetric() {
+    let popupDiv = document.querySelector('.popup');
+    let fullscreenDiv = document.querySelector('.fullscreen');
+
+    popupDiv.innerHTML = `
+        <dialog open class='popupDialog' id="add-metric-window">
+            <div class="popup-title">
+                <span>Choose a metric</span>
+                <div class="small-icon" id="close-button">
+                    <span class="material-symbols-rounded">close</span>
+                </div>
+            </div>
+            <div class="metric-picker">
+                <div class="categories">
+                    <div class="text-button category selected">
+                        <div class="button-text">
+                            All
+                        </div>
+                    </div>
+
+                    <div class="divider"></div>
+
+                    <div class="text-button category">
+                        <div class="button-text">
+                            Project Progress
+                        </div>
+                    </div>
+                    <div class="text-button category">
+                        <div class="button-text">
+                            Employee Workload
+                        </div>
+                    </div>
+                    <div class="text-button category">
+                        <div class="button-text">
+                            Time Based Metrics
+                        </div>
+                    </div>
+                </div>
+
+                <div class="split-area">
+                    <div class="metrics-container">
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    pie_chart
+                                </span>
+                            </div>
+                            <div class="title">
+                                Completion
+                            </div>
+                        </div>
+
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    stacked_bar_chart
+                                </span>
+                            </div>
+                            <div class="title">
+                                Employee Workload
+                            </div>
+                        </div>
+
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    bar_chart
+                                </span>
+                            </div>
+                            <div class="title">
+                                Man Hours per Project
+                            </div>
+                        </div>
+
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    data_table
+                                </span>
+                            </div>
+                            <div class="title">
+                                Task List
+                            </div>
+                        </div>
+
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    grouped_bar_chart
+                                </span>
+                            </div>
+                            <div class="title">
+                                Employee Workload
+                            </div>
+                        </div>
+
+                        <div class="metric selected">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    stacked_line_chart
+                                </span>
+                            </div>
+                            <div class="title">
+                                Expected vs Actual Man Hours
+                            </div>
+                        </div>
+
+                        <div class="metric">
+                            <div class="small-icon">
+                                <span class="material-symbols-rounded">
+                                    query_stats
+                                </span>
+                            </div>
+                            <div class="title">
+                                Search Stats
+                            </div>
+                        </div>
+                    </div>
+                    <div class="metric-preview">
+                        <div class="metric-title">
+                            Expected vs Actual Man Hours
+                        </div>
+                        <div class="metric-preview-chart">
+                            <canvas id="metric-preview-chart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-button blue" id="add-button">
+                <div class="button-text">
+                    Add Metric
+                </div>
+            </div>
+        </dialog>`
+
+    fullscreenDiv.style.filter = 'brightness(0.75)';
+
+    let dialog = popupDiv.querySelector('.popupDialog');
+    dialog.style.transform = 'translateY(0px)'
+    dialog.style.opacity = '1';
+
+    //preview chart
+    let metricPreviewCanvas = document.getElementById("metric-preview-chart")
+    let metricPreviewChart = new Chart(metricPreviewCanvas, {
+        type: 'line',
+        data: {
+            labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'],
+            datasets: [{
+                label: 'Expected Man Hours',
+                data: [40, 50, 45, 60, 55],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }, {
+                label: 'Actual Man Hours',
+                data: [35, 55, 40, 65, 50],
+                fill: false,
+                borderColor: 'rgb(255, 99, 132)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+
+    let categories = dialog.querySelectorAll('.category')
+    categories.forEach(category => {
+        category.addEventListener('click', (event) => {
+            categories.forEach(category => {
+                category.classList.remove('selected')
+            })
+            category.classList.add('selected')
+        })
+    })
+
+    let metrics = dialog.querySelectorAll('.metric')
+    metrics.forEach(metric => {
+        metric.addEventListener('click', (event) => {
+            metrics.forEach(metric => {
+                metric.classList.remove('selected')
+            })
+            metric.classList.add('selected')
+        })
+    })
+    
+    let addButton = dialog.querySelector('#add-button');
+    let closeButton = dialog.querySelector('#close-button');
+
+    addButton.addEventListener('click', async (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        dialog.style.transform = 'translateY(-1%)'
+        dialog.style.opacity = '0';
+        dialog.style.display = 'none';
+        fullscreenDiv.style.filter = 'none';
+        console.log("[addTaskCreateButton] resolving")
+    })
+
+    closeButton.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        dialog.style.transform = 'translateY(-1%)'
+        dialog.style.opacity = '0';
+        dialog.style.display = 'none';
+        fullscreenDiv.style.filter = 'none';
+        console.log("[addTaskCloseButton] rejecting")
+    });
+}
+
+
 
 const dashboard = new Dashboard()
 
 gridExpand.addEventListener("click", () => {
     dashboard.increaseColumns()
+    gridUndo.classList.remove("disabled")
+    gridActionsQueue.push("increase")
 })
 
 gridShrink.addEventListener("click", () => {
     dashboard.decreaseColumns()
+    gridUndo.classList.remove("disabled")
+    gridActionsQueue.push("decrease")
+
 })
 
-gridReset.addEventListener("click", () => {
+gridUndo.addEventListener("click", () => {
     dashboard.resetColumns()
+    gridUndo.classList.add("disabled")
 })
 
-// for (let chart of charts) {
-//     chart.options.animation.duration = 0
-//     chart.update()
-//     console.log(chart.options.animation)
-// }
-// setTimeout(() => {
-//     for (let chart of charts) {
-//         chart.options.animation.duration = 100
-//         chart.update()
-//     }
-// }, 1000)
+viewScroll.addEventListener("click", () => {
+    viewFit.classList.toggle("norender")
+    viewScroll.classList.toggle("norender")
+    dashboardContainer.classList.toggle("scroll")
+    gridUndo.classList.remove("disabled")
+})
+
+viewFit.addEventListener("click", () => {
+    viewFit.classList.toggle("norender")
+    viewScroll.classList.toggle("norender")
+    dashboardContainer.classList.toggle("scroll")
+    gridUndo.classList.remove("disabled")
+})
+
+addMetricButton.addEventListener("click", () => {
+    addMetric()
+})
