9499079af3a4cb9b7ab37149141c5219c17ce6b4
Rally <59124512+TRGRally@users.noreply.github.com>
2024-02-12 21:26:42 +0000

    style register page

---
+++ web/settings/client.js
@@ -0,0 +1,45 @@
+const accountTab = document.querySelector('.account');
+const preferencesTab = document.querySelector('.preferences');
+const systemTab = document.querySelector('.system');
+
+const tabs = document.querySelectorAll('.tab');
+
+const accountOptions = document.querySelector('.account-options');
+const preferencesOptions = document.querySelector('.preferences-options');
+const systemOptions = document.querySelector('.system-options');
+
+accountTab.addEventListener('click', () => {
+    accountOptions.classList.remove('norender');
+    preferencesOptions.classList.add('norender');
+    systemOptions.classList.add('norender');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active-tab');
+    })
+
+    accountTab.classList.add('active-tab');
+})
+
+preferencesTab.addEventListener('click', () => {
+    accountOptions.classList.add('norender');
+    preferencesOptions.classList.remove('norender');
+    systemOptions.classList.add('norender');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active-tab');
+    })
+
+    preferencesTab.classList.add('active-tab');
+})
+
+systemTab.addEventListener('click', () => {
+    accountOptions.classList.add('norender');
+    preferencesOptions.classList.add('norender');
+    systemOptions.classList.remove('norender');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active-tab');
+    })
+
+    systemTab.classList.add('active-tab');
+})
