077bcc697a6a1fcc70e3086600d630678d07d79b
Rally <59124512+TRGRally@users.noreply.github.com>
2024-02-13 17:33:05 +0000

    render personal to do list from backend

--- web/personal/client.js
+++ web/personal/client.js
@@ -14,17 +14,74 @@ import * as global from "../global-ui.js";
 
 //important shit
 var globalPersonalsList = []
+const activeList = document.getElementById('active-list')
+const completedList = document.getElementById('completed-list')
 
 async function getPersonals() {
-    const res = await get_api(`/employee/employee.php/personals`);
+    const res = await get_api(`/employee/employee.php/personals`)
     if (res.success === true) {
         console.log(`[getPersonals] Personals fetched`)
-        globalPersonalsList = res.data
+        globalPersonalsList = res.data.personals
+        console.log(globalPersonalsList)
     }
+}
+
+function selectPersonal(id) {
+    const personal = document.getElementById(id)
+    const personalCards = document.querySelectorAll('.personal-task')
+    personalCards.forEach(card, () => {
+        card.classList.remove('selected')
+    })
+    personal.classList.add('selected')
+}
+
+function renderPersonal(id) {
+    const personal = globalPersonalsList.find(personal => personal.itemID === id)
+    const personalCard = document.createElement('div')
+    personalCard.classList.add('personal-task')
+
+    const checkedState = (personal.state === true) ? 'checked' : '';
+
+    personalCard.innerHTML = `
+        <div class="personal-checkbox">
+            <input type="checkbox" id="${id}" ${checkedState}>
+            <label for="task-2" class="checkbox-label"></label>
+        </div>
+        <div class="personal-content">
+            <div class="personal-title">
+                <div class="title-text">
+                    ${personal.title}
+                </div>
+            </div>
+            <div class="personal-icons">
+                <div class="icon-button no-box edit">
+                    <div class="button-icon">
+                        <span class="material-symbols-rounded">edit</span>
+                    </div>
+                </div>
+                <div class="icon-button no-box delete">
+                    <div class="button-icon">
+                        <span class="material-symbols-rounded">delete</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+
+    if (personal.state === 1) {
+        completedList.appendChild(personalCard)
+    } else {
+        activeList.appendChild(personalCard)
+    }
+
 }
 
 
 
-getPersonals()
+getPersonals().then(() => {
+    globalPersonalsList.forEach(personal => {
+        renderPersonal(personal.itemID)
+    })
+})
 
-global.setBreadcrumb(["My List"], ["./"]);
+global.setBreadcrumb(["My List"], ["./"])
