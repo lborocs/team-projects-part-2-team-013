19c5699082c7fd235456281aa13246de5afe476d
Rally <59124512+TRGRally@users.noreply.github.com>
2024-02-13 17:11:11 +0000

    fetch personals from backend and keep them in array

--- web/personal/client.js
+++ web/personal/client.js
@@ -1,13 +1,30 @@
 import * as global from "../global-ui.js";
 
+/* PERSONALS FORMAT:
+{
+    assignedTo: {
+        empID: '32c2b4e29490c2a226c2b4e29490c2a2'
+    }
+    content: null
+    itemID: "530cc2b4e29490c2a2244bc2b4e29490"
+    state: 0
+    title: "Meet Linda for coffee"
+}
+*/
 
 //important shit
 var globalPersonalsList = []
 
+async function getPersonals() {
+    const res = await get_api(`/employee/employee.php/personals`);
+    if (res.success === true) {
+        console.log(`[getPersonals] Personals fetched`)
+        globalPersonalsList = res.data
+    }
+}
 
 
 
-
-
+getPersonals()
 
 global.setBreadcrumb(["My List"], ["./"]);
