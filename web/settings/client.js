c750f877e3fd9edcd84b7b45124d690609d3d34e
DamonFA <faris_danial84@yahoo.com>
2024-02-14 03:19:26 +0000

    modal and popup styling

--- web/settings/client.js
+++ web/settings/client.js
@@ -193,6 +193,85 @@ sortTasksDirectionMenu.forEach((option) => {
     })
 })
 
+let deleteAccountButton = document.querySelector('.delete-account');
+deleteAccountButton.addEventListener('click', () => {
+    confirmDelete()
+        .then(() => {
+            window.location.href = '../';
+        })
+        .catch((error) => {
+            console.log('Delete account cancelled');
+        });
+});
+
+
+
+
+function confirmDelete() {
+    return new Promise((resolve, reject) => {
+
+        if (global.queryModalSkip()) {
+            resolve();
+            return;
+        }
+
+        let popupDiv = document.querySelector('.popup');
+        let fullscreenDiv = document.querySelector('.fullscreen');
+
+        popupDiv.innerHTML = `
+            <dialog open class='popup-dialog'>
+                <div class="popup-title">
+                    Delete Todo Item
+                    <div class="small-icon close-button">
+                        <span class="material-symbols-rounded">
+                            close
+                        </span>
+                    </div>
+                </div>
+                <div class="popup-text">Are you sure you want to delete this Todo item?</div>
+                <div class="popup-text">This action cannot be undone.</div>
+
+                <div class="popup-buttons">
+                    <div class="text-button" id="cancel-button">
+                        <div class="button-text">Cancel</div>
+                    </div>
+                    <div class="text-button red" id="delete-button">
+                        <div class="button-text">Delete</div>
+                    </div>
+                </div>
+                <span>Tip: you can hold SHIFT to skip this modal</span>
+            </dialog>
+        `;
+        fullscreenDiv.style.filter = 'brightness(0.75)';
+
+        let dialog = popupDiv.querySelector('.popup-dialog');
+        let closeButton = dialog.querySelector('.close-button');
+        let cancelButton = dialog.querySelector('#cancel-button');
+        let deleteButton = dialog.querySelector('#delete-button');
+
+        closeButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            dialog.style.display = 'none';
+            fullscreenDiv.style.filter = 'none';
+            reject();
+        });
+
+        cancelButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            dialog.style.display = 'none';
+            fullscreenDiv.style.filter = 'none';
+            reject();
+        });
+
+        deleteButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            dialog.style.display = 'none';
+            fullscreenDiv.style.filter = 'none';
+            resolve();
+        });
+    });
+}
+
 
 global.setBreadcrumb(["Settings", "Account"], ["../", '#' + "account"])
 
