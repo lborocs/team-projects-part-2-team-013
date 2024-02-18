4b89b87bda3735de26b9debf60f421f531019ec2
TRGRally <jamiespare1@gmail.com>
2024-02-18 17:26:39 +0000

    removed superscript ordinals from full date formatting
    This makes the table much more readable

--- web/global-ui.js
+++ web/global-ui.js
@@ -513,16 +513,8 @@ export function formatDateFull(date) {
     let day = date.getDate();
     let month = date.getMonth();
     let year = date.getFullYear();
-    let ordinal = "th";
-    if (day == 1 || day == 21 || day == 31) {
-        ordinal = "st";
-    } else if (day == 2 || day == 22) {
-        ordinal = "nd";
-    } else if (day == 3 || day == 23) {
-        ordinal = "rd";
-    }
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",  "November", "December"];
-    let formattedDate = `${day}<sup>${ordinal} </sup> ${months[month]} ${year}`;
+    let formattedDate = `${day} ${months[month]} ${year}`;
     return formattedDate;
 }
 
