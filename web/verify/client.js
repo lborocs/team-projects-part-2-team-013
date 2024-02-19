fa071cf3b1c96266d426c77cd2238aa88451a33f
Olive00000 <oliverh.604@gmail.com>
2024-02-19 10:38:44 +0000

    Created verify page

---
+++ web/verify/client.js
@@ -0,0 +1,64 @@
+import * as global from "../global-ui.js"
+
+
+const emailInput = document.getElementById('email');
+const registerButton = document.getElementById('register');
+let statusElement = document.getElementById("status");
+
+registerButton.addEventListener("click", verify)
+
+emailInput.addEventListener('input', function() {    
+    sleep(10).then(() => {
+        console.log(emailInput.value)
+        if(emailInput.value.length > 0) {
+            registerButton.classList.remove("disabled");
+        }
+        else {
+            registerButton.classList.add("disabled");
+        }
+    });
+});
+
+emailInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        verify();
+    }
+});
+
+const sleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+function setStatus(status) {
+    statusElement.innerText = status;
+}
+
+async function verify() {
+    const email = emailInput.value;
+
+    if (email == "") {
+        statusElement.classList.add("status-incorrect");
+        statusElement.classList.remove("hidden");
+        setStatus("Please enter your Email");
+        return false;
+    }
+    let data = {
+        email: email
+    }
+    console.log(data)
+    const res = await post_api("/employee/session.php/verifyemail", data)
+
+    if (res.success) {
+        statusElement.classList.remove("status-incorrect");
+        statusElement.classList.remove("hidden");
+        setStatus("Email sent, please check your email.");
+    }
+    else {
+        console.log("[register] failed to register")
+        statusElement.classList.add("status-incorrect");
+        statusElement.classList.remove("hidden");
+        setStatus(`${res.error.message} (${res.error.code})`);
+    }
+}
